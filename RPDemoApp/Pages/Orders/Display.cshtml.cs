8c0bf605aac6d1153ac22cb0aff044a30cfee8a5
johnmorris4 <johnmorris4@bellsouth.net>
2020-07-05 03:34:57 -0400

    Initial Creation of the files

---
+++ RPDemoApp/Pages/Orders/Display.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RPDemoApp.Pages.Orders
+{
+    public class DisplayModel : PageModel
+    {
+        private readonly IOrderData _orderData;
+        private readonly IFoodData _foodData;
+
+        public DisplayModel(IOrderData orderData, IFoodData foodData)
+        {
+            _orderData = orderData;
+            _foodData = foodData;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
